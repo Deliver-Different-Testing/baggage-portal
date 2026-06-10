313125fadb4e54a59be266dbf34dd7d534ee6e1b
jacobt <jacob@urgent.co.nz>
2026-06-10 17:28:32 +1200

    Remove unused NotificationStatus

--- src/BaggageDelivery.Core/Models/Constants.cs
+++ src/BaggageDelivery.Core/Models/Constants.cs
@@ -1,13 +1,6 @@
  namespace BaggageDelivery.Core.Models;
 
- public static class NotificationStatus
-{
-    public const string Pending = "Pending";
-    public const string Sent = "Sent";
-    public const string Failed = "Failed";
-}
-
-public static class NotificationChannel
+ public static class NotificationChannel
 {
     public const string Sms = "sms";
     public const string Email = "email";
